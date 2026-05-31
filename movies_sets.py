8d552525f6aa95a0e50033b174f157c586aa7da1
sriramsai1101 <sriramsaidudam1101@gmail.com>
2026-05-31 21:29:43 +0530

    Add files via upload

---
+++ movies_sets.py
@@ -0,0 +1,11 @@
+watched = {"RRR", "Pushpa", "KGF", "Baahubali"}
+friend_watched = {"Pushpa", "KGF", "Salaar", "Leo"}
+#both
+both=watched&friend_watched
+print(both)
+#combine
+ours=watched|friend_watched
+print(ours)
+#or
+by_me=watched-friend_watched
+print(by_me)

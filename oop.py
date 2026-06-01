3946d3513365a3873b130e78e638f775e5e006c5
sriramsai1101 <sriramsaidudam1101@gmail.com>
2026-06-01 16:04:27 +0530

    Add files via upload

--- oop.py
+++ oop.py
@@ -1,13 +1,17 @@
-#class is an blueprint
 class student:
-    def __init__(self,name,college):
+    def __init__(self,name,college,branch,year):
         self.name=name
         self.college=college
+        self.branch=branch
+        self.year=year
     def introduce(self):
-        print("my name is " ,self.name)
-        print("my college id : ",self.college)
-student1=student("sriram","avanthi")
-student2=student("eshwar","jntuh")
-student1.introduce()       
-
+        print("my name is : ",self.name)
+        print("my college: ",self.college)
+        print("iam from: ",self.branch)
+        print("from: ",self.year)
+student1=student("sriram","avanthi","ai&ml","3rd year")
+student2=student("eshwar","vivekanadha","degree","3rd year")
+student3=student("rakesh","DRK","csm","3rd year")
+student1.introduce()
 student2.introduce()
+student3.introduce()

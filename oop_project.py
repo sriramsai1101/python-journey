de39da7e162fc33b0f2b321e5321e86515b34219
sriramsai1101 <sriramsaidudam1101@gmail.com>
2026-06-03 16:08:15 +0530

    Add files via upload

--- oop_project.py
+++ oop_project.py
@@ -7,15 +7,21 @@ class bank:
         
         print("deposited: ",amount)
         print("current balance: ",amount+self.__balance)
-        self.__balance=self.__balance+amount
+        self.__balance = self.__balance + amount
+
     def withdraw(self,cash):
-        print("withdral: ",cash)
-        print("current balance: ",self.__balance-cash)    
-        self.__balance=self.__balance-cash
+        if cash<self.__balance:
+          print("withdral: ",cash)
+          print("current balance: ",self.__balance-cash)
+          self.__balance = self.__balance - cash 
+
+        else:
+         print("your cant withdraw that ammount becauseyour current balance is : ",self.__balance)    
+        
 s=bank()
 s.show_balance()
 s.deposit(1000)
-s.withdraw(2000)
+s.withdraw(600000)
                 
        
 

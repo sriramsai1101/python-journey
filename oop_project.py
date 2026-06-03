16ee356870ceda1522e6c5fa2e5abdd052de44ea
sriramsai1101 <sriramsaidudam1101@gmail.com>
2026-06-03 13:08:34 +0530

    Add files via upload

---
+++ oop_project.py
@@ -0,0 +1,21 @@
+class bank:
+    def __init__(self):
+        self.__balance=50000
+    def show_balance(self):
+        print("balance:",self.__balance)
+    def deposit(self,amount):
+        
+        print("deposited: ",amount)
+        print("current balance: ",amount+self.__balance)
+        self.__balance=self.__balance+amount
+    def withdraw(self,cash):
+        print("withdral: ",cash)
+        print("current balance: ",self.__balance-cash)    
+        self.__balance=self.__balance-cash
+s=bank()
+s.show_balance()
+s.deposit(1000)
+s.withdraw(2000)
+                
+       
+

e83c10c4008a5641c55b3f95e041d6b185885676
sriramsai1101 <sriramsaidudam1101@gmail.com>
2026-06-05 22:22:40 +0530

    Add files via upload

---
+++ numpy_.arrays.py
@@ -0,0 +1,7 @@
+import numpy as np
+numbers=np.array([10,20,30,40,50])
+print("arrays: ",numbers)
+print("sum: ",np.sum(numbers))
+print("average:",np.mean(numbers))
+print("max: ",np.max(numbers))
+print("min: ",np.min(numbers))

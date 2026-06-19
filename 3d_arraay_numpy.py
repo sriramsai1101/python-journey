97af82b212c645f47e0b2cff049ad5d19c669446
sriramsai1101 <sriramsaidudam1101@gmail.com>
2026-06-19 19:16:56 +0530

    Add files via upload

---
+++ 3d_arraay_numpy.py
@@ -0,0 +1,17 @@
+import numpy as np
+arr=np.array([
+    [
+        [10,20,30],
+        [40,50,60]
+
+    ],
+    [
+        [70,80,90],
+        [100,110,120]
+    ]
+]
+)
+print(arr)
+print(arr.shape)
+print(arr.size)
+print(arr[0][0][1])

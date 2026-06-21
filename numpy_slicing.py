65399956397c9f818b28c36765d60371f5099aae
sriramsai1101 <sriramsaidudam1101@gmail.com>
2026-06-21 18:06:37 +0530

    Add files via upload

---
+++ numpy_slicing.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print(arr[1:3]) #slicing
+#operations
+print(arr+5)
+print(arr/5)
+print(arr*5)
+print(np.max(arr))
+print(np.min(arr))
+print(np.mean(arr))
